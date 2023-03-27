9a7f318438b61558485d420be86cb24f4eb93f26
Amal MK <amalmk@Amals-MacBook-Air.local>
2023-03-27 17:43:23 +0530

    setting up sanity

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+  modules: ["@nuxtjs/sanity"],
+  sanity: {
+    projectId: "",
+  },
+  css: ["~/assets/stylesheets/main.scss"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+});
